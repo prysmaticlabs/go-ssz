e78af59c1a1345040c4bedbae21b046ab86894bf
Preston Van Loon <preston@prysmaticlabs.com>
2019-05-29 11:50:30 -0400

    Initial commit

---
+++ deps.bzl
@@ -0,0 +1,97 @@
+"""
+go-ssz dependencies.
+
+Add go_repository dependencies where with the _maybe function so that downstream
+bazel projects' go_repository take precidence over the commits specified here.
+
+Add the license comment to each dependency for quick analysis of the licensing
+requirements for this project.
+"""
+load("@bazel_gazelle//:deps.bzl", "go_repository")
+
+def go_ssz_dependencies():
+    _maybe(
+        # MIT License
+        go_repository,
+        name = "com_github_karlseguin_ccache",
+        commit = "ec06cd93a07565b373789b0078ba88fe697fddd9",
+        importpath = "github.com/karlseguin/ccache",
+    )
+
+    _maybe(
+        # Apache License 2.0
+        go_repository,
+        name = "com_github_prometheus_client_golang",
+        commit = "662e8a9ffaaa74a4d050023c2cb26902cd9bab63",
+        importpath = "github.com/prometheus/client_golang",
+    )
+
+    _maybe(
+        # Apache License 2.0
+        go_repository,
+        name = "com_github_prometheus_common",
+        commit = "1ba88736f028e37bc17328369e94a537ae9e0234",
+        importpath = "github.com/prometheus/common",
+    )
+
+    _maybe(
+        # Apache License 2.0
+        go_repository,
+        name = "com_github_prometheus_client_model",
+        commit = "fd36f4220a901265f90734c3183c5f0c91daa0b8",
+        importpath = "github.com/prometheus/client_model",
+    )
+
+    _maybe(
+        # Apache License 2.0
+        go_repository,
+        name = "com_github_prometheus_procfs",
+        commit = "bbced9601137e764853b2fad7ec3e2dc4c504e02",
+        importpath = "github.com/prometheus/procfs",
+    )
+
+    _maybe(
+        # Apache License 2.0
+        go_repository,
+        name = "com_github_matttproud_golang_protobuf_extensions",
+        commit = "c12348ce28de40eed0136aa2b644d0ee0650e56c", 
+        importpath = "github.com/matttproud/golang_protobuf_extensions",
+    )
+
+    _maybe(
+        # MIT License
+        go_repository,
+        name = "com_github_beorn7_perks",
+        commit = "4ded152d4a3e2847f17f185a27b2041ae7b63979",
+        importpath = "github.com/beorn7/perks",
+    )
+
+    _maybe(
+        # GNU Lesser General Public License v3.0
+        go_repository,
+        name = "com_github_ethereum_go_ethereum",
+        commit = "099afb3fd89784f9e3e594b7c2ed11335ca02a9b",
+        importpath = "github.com/ethereum/go-ethereum",
+        # Note: go-ethereum is not bazel-friendly with regards to cgo. We have a
+        # a fork that has resolved these issues by disabling HID/USB support and
+        # some manual fixes for c imports in the crypto package. This is forked
+        # branch should be updated from time to time with the latest go-ethereum
+        # code.
+        remote = "https://github.com/prysmaticlabs/bazel-go-ethereum",
+        vcs = "git",
+    )
+
+    _maybe(
+        # Permissive license
+        # https://github.com/golang/crypto/blob/master/LICENSE
+        go_repository,
+        name = "org_golang_x_crypto",
+        commit = "8dd112bcdc25174059e45e07517d9fc663123347",
+        importpath = "golang.org/x/crypto",
+    )
+
+
+
+def _maybe(repo_rule, name, **kwargs):
+    if name not in native.existing_rules():
+        repo_rule(name = name, **kwargs)
---
+++ hash_cache_test.go
@@ -0,0 +1,153 @@
+package ssz
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+	"reflect"
+	"testing"
+	"time"
+)
+
+type junkObject struct {
+	D2Int64Slice [][]uint64
+	Uint         uint64
+	Int64Slice   []uint64
+}
+
+type tree struct {
+	First  []*junkObject
+	Second []*junkObject
+}
+
+func generateJunkObject(size uint64) []*junkObject {
+	object := make([]*junkObject, size)
+	for i := uint64(0); i < uint64(len(object)); i++ {
+		d2Int64Slice := make([][]uint64, size)
+		is := make([]uint64, size)
+		uInt := uint64(time.Now().UnixNano())
+		is[i] = i
+		d2Int64Slice[i] = make([]uint64, size)
+		for j := uint64(0); j < uint64(len(object)); j++ {
+			d2Int64Slice[i][j] = i + j
+		}
+		object[i] = &junkObject{
+			D2Int64Slice: d2Int64Slice,
+			Uint:         uInt,
+			Int64Slice:   is,
+		}
+
+	}
+	return object
+}
+
+func TestObjCache_byHash(t *testing.T) {
+	cache := newHashCache(100000)
+	byteSl := [][]byte{{0, 0}, {1, 1}}
+	mr, err := merkleHash(byteSl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	hs, err := hashedEncoding(reflect.ValueOf(byteSl))
+	if err != nil {
+		t.Fatal(err)
+	}
+	exists, _, err := cache.RootByEncodedHash(ToBytes32(hs))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Error("Expected block info not to exist in empty cache")
+	}
+	if _, err := cache.MerkleHashCached(byteSl); err != nil {
+		t.Fatal(err)
+	}
+	exists, fetchedInfo, err := cache.RootByEncodedHash(ToBytes32(hs))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Error("Expected blockInfo to exist")
+	}
+	if !bytes.Equal(mr, fetchedInfo.MerkleRoot) {
+		t.Errorf(
+			"Expected fetched info number to be %v, got %v",
+			mr,
+			fetchedInfo.MerkleRoot,
+		)
+	}
+	if fetchedInfo.Hash != ToBytes32(hs) {
+		t.Errorf(
+			"Expected fetched info hash to be %v, got %v",
+			hs,
+			fetchedInfo.Hash,
+		)
+	}
+}
+
+func TestMerkleHashWithCache(t *testing.T) {
+	cache := newHashCache(100000)
+	for i := 0; i < 200; i++ {
+		runMerkleHashTests(t, func(val [][]byte) ([]byte, error) {
+			return merkleHash(val)
+		})
+	}
+	for i := 0; i < 200; i++ {
+		runMerkleHashTests(t, func(val [][]byte) ([]byte, error) {
+			return cache.MerkleHashCached(val)
+		})
+	}
+}
+
+func BenchmarkHashWithoutCache(b *testing.B) {
+	useCache = false
+	First := generateJunkObject(100)
+	TreeHash(&tree{First: First, Second: First})
+	for n := 0; n < b.N; n++ {
+		TreeHash(&tree{First: First, Second: First})
+	}
+}
+
+func BenchmarkHashWithCache(b *testing.B) {
+	useCache = true
+	First := generateJunkObject(100)
+	type tree struct {
+		First  []*junkObject
+		Second []*junkObject
+	}
+	TreeHash(&tree{First: First, Second: First})
+	for n := 0; n < b.N; n++ {
+		TreeHash(&tree{First: First, Second: First})
+	}
+}
+
+func TestBlockCache_maxSize(t *testing.T) {
+	maxCacheSize := int64(10000)
+	cache := newHashCache(maxCacheSize)
+	for i := uint64(0); i < uint64(maxCacheSize+1025); i++ {
+
+		if err := cache.AddRoot(ToBytes32(Bytes4(i)), []byte{1}); err != nil {
+			t.Fatal(err)
+		}
+	}
+	log.Printf(
+		"hash cache key size is %d, itemcount is %d",
+		maxCacheSize,
+		cache.hashCache.ItemCount(),
+	)
+	time.Sleep(1 * time.Second)
+	if int64(cache.hashCache.ItemCount()) > maxCacheSize {
+		t.Errorf(
+			"Expected hash cache key size to be %d, got %d",
+			maxCacheSize,
+			cache.hashCache.ItemCount(),
+		)
+	}
+}
+
+// Bytes4 returns integer x to bytes in little-endian format, x.to_bytes(4, 'big').
+func Bytes4(x uint64) []byte {
+	bytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytes, x)
+	return bytes[:4]
+}
