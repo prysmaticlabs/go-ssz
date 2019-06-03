7e638fe9a6f1182846da473ad4b8832ce79ebb33
Raul Jordan <raul@prysmaticlabs.com>
2019-06-03 11:27:23 -0500

    pack function

--- helpers.go
+++ helpers.go
@@ -7,23 +7,39 @@ import (
 
 // Given ordered objects of the same basic type, serialize them, pack them into BYTES_PER_CHUNK-byte
 // chunks, right-pad the last chunk with zero bytes, and return the chunks.
+// Basic types are either bool, or uintN where N = {8, 16, 32, 64, 128, 256}.
 func pack(objects []interface{}) ([][]byte, error) {
-	// We use a bytes.Buffer as our io.Writer.
-	buffer := new(bytes.Buffer)
-	// ssz.Encode writes the encoded data to the buffer.
-	if err := ssz.Encode(buffer, objects); err != nil {
-		return nil, err
+    serializedItems := make([][]byte, len(objects))
+    for i, item := range objects {
+		// We use a bytes.Buffer as our io.Writer.
+		buffer := new(bytes.Buffer)
+		// ssz.Encode writes the encoded data to the buffer.
+		if err := ssz.Encode(buffer, item); err != nil {
+			return nil, err
+		}
+		serializedItems[i] = buffer.Bytes()
 	}
-	encodedBytes := buffer.Bytes()
 	chunks := [][]byte{}
-	if len(encodedBytes) == 0 {
+	numItems := len(serializedItems)
+	if numItems == 0 {
 		emptyChunk := make([]byte, BytesPerChunk)
 		return [][]byte{emptyChunk}, nil
-	} else if len(encodedBytes) == BytesPerChunk {
-		return [][]byte{encodedBytes}, nil
+	} else if len(serializedItems[0]) == BytesPerChunk {
+		return serializedItems, nil
 	}
-	// Otherwise, we pack items into chunks and pad if necessary.
-	return [][]byte{}, nil
+	itemsPerChunk := sszChunkSize / len(serializedItems[0])
+	for i := 0; i < numItems; i += itemsPerChunk {
+		chunk := make([]byte, 0)
+		j := i + itemsPerChunk
+		if j > numItems {
+			j = numItems
+		}
+		for _, item := range serializedItems[i:j] {
+			chunk = append(chunk, item...)
+		}
+		chunks = append(chunks, chunk)
+	}
+	return chunks, nil
 }
 
 // Given ordered BYTES_PER_CHUNK-byte chunks, if necessary append zero chunks so that the
