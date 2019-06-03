d99e7d43aca761d5c61d740dbb79828267c3565f
Raul Jordan <raul@prysmaticlabs.com>
2019-06-03 10:36:18 -0500

    comments lint

--- config.go
+++ config.go
@@ -5,8 +5,12 @@ import (
 )
 
 var (
-	BytesPerChunk = 32 // BytesPerChunk for an SSZ serialized object.
-	BytesPerLengthOffset = 4 // BytesPerLengthOffset defines a constant for off-setting serialized chunks.
-	BitsPerByte = 8 // BitsPerByte as a useful constant.
-	ErrNotMerkleRoot = errors.New("object is not a Merkle root") // ErrNotMerkleRoot when a cached item is not root.
+	// BytesPerChunk for an SSZ serialized object.
+	BytesPerChunk = 32
+	// BytesPerLengthOffset defines a constant for off-setting serialized chunks.
+	BytesPerLengthOffset = 4
+	// BitsPerByte as a useful constant.
+	BitsPerByte = 8
+	// ErrNotMerkleRoot when a cached item is not root.
+	ErrNotMerkleRoot = errors.New("object is not a Merkle root")
 )
