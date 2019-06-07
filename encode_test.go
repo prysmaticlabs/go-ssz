891b2c160a416a1b6f0e213bbfc8a273a7955be2
Raul Jordan <raul@prysmaticlabs.com>
2019-06-07 09:19:46 -0500

    test build pass lint

--- encode_test.go
+++ encode_test.go
@@ -34,9 +34,7 @@ func TestEncode(t *testing.T) {
 		t.Errorf("encode() = %v, want %v", encodedBytes, want)
 	}
 	prevRoot, err := hex.DecodeString("e8933c7bb4e15a6476373346d2334d8f845bc3c0c93d5d5acf3fd0fba9d7e8d9")
-	t.Log(err)
 	root, err := hex.DecodeString("0f9e7e66592424d43d7d6109182b6519c0b748e6eb33cbccc1527aae78dc889f")
-	t.Log(err)
 	cross := &crosslink{
 		PreviousCrosslinkRoot: prevRoot,
 		CrosslinkDataRoot:     root,
