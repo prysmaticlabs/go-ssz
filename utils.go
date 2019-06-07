4c1b87ef15c4085f7858d7ba65465fcf797174b4
Raul Jordan <raul@prysmaticlabs.com>
2019-06-07 13:05:29 -0500

    change back hash func

--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package ssz
 
-import "golang.org/x/crypto/sha3"
+import "crypto/sha256"
 
 // ToBytes32 is a convenience method for converting a byte slice to a fix
 // sized 32 byte array. This method will truncate the input if it is larger
@@ -11,18 +11,19 @@ func ToBytes32(x []byte) [32]byte {
 	return y
 }
 
-// Hash defines a function that returns the
-// Keccak-256/SHA3 hash of the data passed in.
+// Hash defines a function that returns the sha256 hash of the data passed in.
 // https://github.com/ethereum/eth2.0-specs/blob/master/specs/core/0_beacon-chain.md#appendix
 func Hash(data []byte) [32]byte {
 	var hash [32]byte
-	h := sha3.NewLegacyKeccak256()
 
+	h := sha256.New()
 	// The hash interface never returns an error, for that reason
 	// we are not handling the error below. For reference, it is
 	// stated here https://golang.org/pkg/hash/#Hash
+
 	// #nosec G104
 	h.Write(data)
 	h.Sum(hash[:0])
+
 	return hash
 }
